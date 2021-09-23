a0286e05a3f9d0965a0ad218c551483361485558
Bogdan <bogdandadayan@gmail.com>
2021-09-24 01:08:55 +0300

    up

---
+++ GOODS.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodsCheck
+{
+    public class GOODS : INotifyPropertyChanged
+    {
+        [Key]
+        public int GOODS_ID { get; set; }
+        public int CATEGORY_ID { get; set; }
+        public string CATEGORY_NAME { get; set; }
+        public string GOODS_NAME { get; set; }
+        public int GOODS_PRICE { get; set; }
+        public string GOODS_DESCRIPTION { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GoodsCheck
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            SetConnection();
+            InitializeComponent();
+        }
+
+
+        private void SetConnection()
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                var goods = db.GOODS.ToList();
+            }
+        }
+
+    }
+}
