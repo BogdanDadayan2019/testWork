ad7e086f92f9bf0de20151a755cd79bf52c08d18
Bogdan <bogdandadayan@gmail.com>
2021-09-24 02:08:20 +0300

    upd

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Oracle.ManagedDataAccess.Client;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,26 +18,52 @@ using System.Windows.Shapes;
 
 namespace GoodsCheck
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
 
+        OracleConnection con = null;
         public MainWindow()
         {
             SetConnection();
+           
             InitializeComponent();
-        }
-
+        }        
 
         private void SetConnection()
-        {
-            using (ApplicationContext db = new ApplicationContext())
+        {         
+            con = new OracleConnection("Data Source=XE;User Id=SYSTEM;Password=name23;");
+            try
             {
-                var goods = db.GOODS.ToList();
+                con.Open();
+            }
+            catch (Exception)
+            {
+
+                throw;
             }
         }
 
+        private void UpdateDataGrid()
+        {
+            OracleCommand cmd = con.CreateCommand();
+            cmd.CommandText = "SELECT GOODS_ID, CATEGORY_ID, CATEGORY_NAME, GOODS_NAME, GOODS_PRICE, GOODS_DESCRIPTION FROM GOODS";
+            cmd.CommandType = CommandType.Text;
+            OracleDataReader dr = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(dr);
+            goodsGrid.ItemsSource = dt.DefaultView;
+            dr.Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            UpdateDataGrid();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            con.Close();
+        }
     }
 }
